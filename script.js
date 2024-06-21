d605f4d25bbfce45690fe2c6a279e7beeb6954fb
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-21 16:40:11 +0530

    JS - Radio Selection & Functionality

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const penaltyTimeEl = document.querySelector('.penalty-time');
 const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
-
+let questionAmount = 0;
 let equationsArray = [];
 
 // Game Page
@@ -35,13 +35,13 @@ const wrongFormat = [];
 // Create Correct/Incorrect Random Equations
 function createEquations() {
   // Randomly choose how many correct equations there should be
-  // const correctEquations = 
+  // const correctEquations =
   // Set amount of wrong equations
-  // const wrongEquations = 
+  // const wrongEquations =
   // Loop through, multiply random numbers up to 9, push to array
   // for (let i = 0; i < correctEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
   //   equationObject = { value: equation, evaluated: 'true' };
@@ -49,13 +49,13 @@ function createEquations() {
   // }
   // Loop through, mess with the equation results, push to array
   // for (let i = 0; i < wrongEquations; i++) {
-  //   firstNumber = 
-  //   secondNumber = 
+  //   firstNumber =
+  //   secondNumber =
   //   const equationValue = firstNumber * secondNumber;
   //   wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
   //   wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
   //   wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-  //   const formatChoice = 
+  //   const formatChoice =
   //   const equation = wrongFormat[formatChoice];
   //   equationObject = { value: equation, evaluated: 'false' };
   //   equationsArray.push(equationObject);
@@ -82,3 +82,34 @@ function createEquations() {
 //   bottomSpacer.classList.add('height-500');
 //   itemContainer.appendChild(bottomSpacer);
 // }
+
+// Get the value from selected radio button
+function getRadioValue() {
+  let radioValue;
+  radioInputs.forEach(radioInput => {
+    if (radioInput.checked) {
+      radioValue = radioInput.value;
+    }
+  });
+  return radioValue;
+}
+
+// From that decides amount if questions
+function selectQuestionAmount(e) {
+  e.preventDefault();
+  questionAmount = getRadioValue();
+  console.log('Question amount:', questionAmount);
+}
+
+startForm.addEventListener('click', () => {
+  radioContainers.forEach(radioEl => {
+    // Remove Slectd Labrl styling
+    radioEl.classList.remove('selected-label');
+    // Add it back if radio input is checked
+    if (radioEl.children[1].checked) {
+      radioEl.classList.add('selected-label');
+    }
+  });
+});
+
+startForm.addEventListener('submit', selectQuestionAmount);
